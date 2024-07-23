3c1e689c58f4904b423b2de46d5d57c5db1f28fa
Chaitra-27 <chaitradbg@gmail.com>
2024-07-23 14:31:33 +0530

    first commit

---
+++ src/Java/PS3.java
@@ -0,0 +1,22 @@
+package Java;
+
+public class PS3 {
+	
+	int a;
+	public PS3(int a) {
+		this.a=a;
+	}
+
+	public int multiplytwo() {
+		a=a*2;
+		
+		return a;
+	}
+	
+	public int multiplythree() {
+		a=a*3;
+		
+		return a;
+	}
+
+}
