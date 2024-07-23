89ae5a2419ab4e0c3c3ad132b7f54c571e7c93e5
Chaitra-27 <chaitradbg@gmail.com>
2024-07-23 19:03:20 +0530

    add a+b

---
+++ src/Java/Add.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		a=5;
+		b=6;
+		
+		System.out.println(a+b);
+
+	}
+
+}
